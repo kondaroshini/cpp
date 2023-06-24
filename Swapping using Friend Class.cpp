9eb155a8a5bf6b1944e9a1625b3d714e3bbb6195
kondaroshini <123068795+kondaroshini@users.noreply.github.com>
2023-06-24 20:37:40 +0530

    Add files via upload

---
+++ Swapping using Friend Class.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+swapping 2 num using friend class
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+class SwapNumbers
+{ 
+	private:
+		int x,y;
+	public:
+	void getdata()
+	{ 
+		cin>>x>>y;
+	}
+	void showdata()
+	{
+	 cout<<"X="<<x<<","<<"Y="<<y;
+	}
+	friend void swap(SwapNumbers&s);
+};
+void swap(SwapNumbers&s)
+{ 
+	int temp;
+	temp=s.x;
+	s.x=s.y;
+	s.y=temp;
+}
+int main()
+{
+	SwapNumbers s;
+	cout<<"Input two numbers to swap:";
+	s.getdata();
+	cout<<endl<<"Before swapping:"<<endl;
+	s.showdata();
+	swap(s);
+	cout<<endl<<"After swapping:"<<endl;
+	s.showdata();
+	return 0;
+}
