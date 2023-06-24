fbcd3d5e99bc5c197127a2dbabd66ed9f6601fe0
kondaroshini <123068795+kondaroshini@users.noreply.github.com>
2023-06-24 19:17:26 +0530

    Add files via upload

---
+++ Explicit Type Casting.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+ program to demonstrate explicit type casting
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   float a=23.56;
+   int sum=(int)a+1;
+   cout<<"sum is= "<<sum<<endl;
+   return 0;
+}
