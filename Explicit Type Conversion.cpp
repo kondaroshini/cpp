99f29af34583be74a0b418a0e0c040067a7fb12e
kondaroshini <123068795+kondaroshini@users.noreply.github.com>
2023-06-24 19:09:52 +0530

    Add files via upload

---
+++ Explicit Type Conversion.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+working on implicit type-conversion
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num_int=130;
+    double num_double;
+    num_double=num_int;
+    cout<<"num_int is:"<<num_int<<endl;
+    cout<<"num_double is:"<<num_double<<endl;
+    return 0;
+}
+
